1cf7400efc6c074758347a1cfafadd1b4c7510d1
Willian-code <willianrosendodelima@outlook.com>
2022-09-09 13:29:14 -0300

    file to be executed and header

--- philo/app/ft_philo.c
+++ philo/app/ft_philo.c
@@ -6,7 +6,13 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 16:30:45 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/08 16:30:46 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/08 16:57:53 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_philo.h"
+
+int	main(int argc, char *argv[])
+{
+	return (0);
+}
--- philo/include/ft_philo.h
+++ philo/include/ft_philo.h
@@ -6,7 +6,18 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 16:30:51 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/08 16:30:52 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/08 17:05:36 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
