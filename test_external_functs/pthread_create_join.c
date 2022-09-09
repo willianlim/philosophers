58d2cc81fa53b1e65eb488201a76f58a48709182
Willian-code <willianrosendodelima@outlook.com>
2022-09-09 13:31:38 -0300

    testing function that creates and synchronizes threads

---
+++ test_external_functs/pthread_create_join.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void	*routine()
+{
+	printf("Test from threads\n");
+	sleep(3);
+	printf("Ending thread\n");
+}
+
+int main(int argc, char *argv[])
+{
+	pthread_t	t1;
+	pthread_t	t2;
+
+	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+		return (1);
+	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
+		return (2);
+	if (pthread_join(t1, NULL))
+		return (3);
+	if (pthread_join(t2, NULL))
+		return (4);
+	return (0);
+}
