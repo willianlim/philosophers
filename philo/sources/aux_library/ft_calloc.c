c164a60f2e583346a65c3bbddb2082011076f3ce
Willian-code <willianrosendodelima@outlook.com>
2022-09-13 20:20:34 -0300

    functions imported from libft

---
+++ philo/sources/aux_library/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 10:59:38 by wrosendo          #+#    #+#             */
+/*   Updated: 2022/09/12 11:04:17 by wrosendo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_philo.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == 0)
+		return (ptr);
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
+}
