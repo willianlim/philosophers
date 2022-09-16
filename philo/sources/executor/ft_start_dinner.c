9073a06893bb51cf6c1657fb7fcaf4d569e510f9
Willian-code <willianrosendodelima@outlook.com>
2022-09-15 22:48:54 -0300

    creating the threads for the philosophers and initializing the routines

--- philo/sources/executor/ft_start_dinner.c
+++ philo/sources/executor/ft_start_dinner.c
@@ -1,52 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_start_dinner.c                                   :+:      :+:    :+:   */
+/*   ft_start_dinner.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 09:50:46 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/13 16:30:50 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/15 22:03:52 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_philo.h"
 
-int	ft_start_dinner(t_meal *meal, t_philo *philo)
+int	ft_start_dinner(t_data *data, t_philosopher *philo)
 {
 	int	i;
 
 	i = -1;
-	puts("1");
-	if (pthread_mutex_lock(&meal->time) || \
-	!ft_stopwatch(&meal->start))
-		return (FALSE);
-	puts("2");
-
-	while (++i < meal->number_of_philo)
+	memset(philo, 0, sizeof(t_philosopher));
+	philo = data->philosopher;
+	ft_stopwatch(&data->starting_stopwatch);
+	while (++i < data->number_of_philo)
 	{
-		puts("entrou no loop");
-		philo[i].i = i;
-		printf("%d\n", philo[i].i);
-		puts("antes do primeiro if");
-		if (pthread_create(&philo[i].thread, NULL, ft_routine, \
-		(void *)(&philo[i])))
+		if (pthread_create(&philo[i].thread, NULL, ft_routine, &philo[i]))
 		{
-			printf("deu ruim no primeiro if\n");
+			printf(RED"Error: Failed to create a thread for philo\n"RESET);
 			return (FALSE);
 		}
-		puts("antes do segundo if");
-		if (pthread_create(&philo[i].monitoring, NULL, ft_monitoring, \
-		(void *)(&philo[i])))
-			return (FALSE);
+		ft_stopwatch(&philo[i].last_meal);
 	}
-	puts("3");
-	i = -1;
-	while (++i < meal->number_of_philo)
-	{
-		if (pthread_join(philo[i].thread, NULL))
-			printf("deu tuim\n");
-	}
-	puts("4");
+	ft_monitoring(data, data->philosopher);
 	return (TRUE);
 }
