eb37bae7cbc2a8ef781f9c461e1d7d200cc975aa
Willian-code <willianrosendodelima@outlook.com>
2022-09-15 22:57:56 -0300

    testing a 'mini philosophers'

--- test_external_functs/dinner.c
+++ test_external_functs/dinner.c
@@ -4,30 +4,41 @@
 # include <unistd.h>
 # include <pthread.h>
 
+# include <string.h>
+
 pthread_t		philosopher[5];
 pthread_mutex_t	chopstick[5];
 
 void	*func(void *arg)
 {
+	int	i;
+	int	iteration;
 	int	n;
 	int	sleeptime;
 
-	n = (long)arg;
-	pthread_mutex_lock(&chopstick[n]);
-	pthread_mutex_lock(&chopstick[(n + 1) % 5]);
-	printf("Philosopher %d is thinking\n", n);
-	printf("Philosopher %d is eating \n", n);
-	sleep(3);
-	sleeptime = rand() % 10;
-	pthread_mutex_unlock(&chopstick[n]);
-	pthread_mutex_unlock(&chopstick[(n + 1) % 5]);
-	printf("Philosopher %d finished eating in %d seconds \n", n, sleeptime);
+	i = -1;
+	iteration = 2;
+	n = *(long *)arg;
+	while (++i < 2)
+	{
+		pthread_mutex_lock(&chopstick[n]);
+		pthread_mutex_lock(&chopstick[(n + 1) % 5]);
+		printf("\nBegin eating: %d with forks %d and %d", n, n, (n + 1) % 5);
+		sleep(3);
+		printf("\nFinish eating: %d", n);
+		sleeptime = rand() % 10;
+		pthread_mutex_unlock(&chopstick[n]);
+		pthread_mutex_unlock(&chopstick[(n + 1) % 5]);
+		printf("\nThinking: %d", n);
+	}
+	free(arg);
 	return (NULL);
 }
 
 int	main(int argc, char *argv[])
 {
 	int	i;
+	int	*arg;
 	srand(time(NULL));
 
 	i = -1;
@@ -35,7 +46,11 @@ int	main(int argc, char *argv[])
 		pthread_mutex_init(&chopstick[i], NULL);
 	i = -1;
 	while (++i < 5)
-		pthread_create(&philosopher[i], NULL, (void *)func, (void *)(long)i);
+	{
+		arg = (int *)malloc(sizeof(int));
+		*arg = i;
+		pthread_create(&philosopher[i], NULL, (void *)func, arg);
+	}
 	i = -1;
 	while (++i < 5)
 		pthread_join(philosopher[i], NULL);
