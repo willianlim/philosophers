0076090ac14d8fdc55454865439269203b2aa5f6
Willian-code <willianrosendodelima@outlook.com>
2022-09-20 21:36:07 -0300

    doing the memory management and destroying the 'mutex'

--- philo/sources/executor/ft_end_dinner.c
+++ philo/sources/executor/ft_end_dinner.c
@@ -6,46 +6,33 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 22:04:43 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/16 13:47:39 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/20 16:29:23 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_philo.h"
 
-int	ft_end_dinner(t_data *data, t_philosopher *philo)
+int	ft_end_dinner(t_rules *rules)
 {
 	int	i;
 
-	printf("entre no end dinner\n");
-	i = 0;
-	while (i < data->number_of_philo)
-	{
-		printf("estou no join\n");
-		pthread_join(philo[i++].thread, NULL);
-	}
+	// i = -1;
+	// while (++i < rules->number_of_philo)
+	// 	pthread_join(rules->philosophers[i].thread_id, NULL);
 	i = -1;
-	while (++i < data->number_of_philo)
-	{
-		printf("estou no destroy\n");
-		pthread_mutex_destroy(&data->fork[i]);
-	}
-	pthread_mutex_destroy(&data->lock_dinner);
-	pthread_mutex_destroy(&data->lock_print);
+	while (++i < rules->number_of_philo)
+		pthread_mutex_destroy(&rules->philosophers[i].check_mutex);
+	i = -1;
+	while (++i < rules->number_of_philo)
+		pthread_mutex_destroy(&rules->forks[i]);
+	pthread_mutex_destroy(&rules->finish_mutex);
 	// i = -1;
-	// while (++i < data->number_of_philo)
+	// while (++i < rules->number_of_philo)
 	// {
-	// 	free(&data->fork);
-	// 	free(&data->philosopher);
+	// 	free(rules->forks);
+	// 	free(rules->philosophers);
 	// }
-	free(data->fork);
-	free(data->philosopher);
-	// free(philo->data->fork);
-	// free(philo->data->philosopher);
-	// i = -1;
-	// while (++i < data->number_of_philo)
-	// {
-	// 	free(&philo[i]);
-	// }
-	printf("Terminei o end dinner\n");
+	free(rules->forks);
+	free(rules->philosophers);
 	return (TRUE);
 }
