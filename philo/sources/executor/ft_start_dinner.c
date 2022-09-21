5cdc3bab70d018ce98d417477dd33696085ce1de
Willian-code <willianrosendodelima@outlook.com>
2022-09-21 20:34:02 -0300

    checking the optional argument for monitoring the threads using the variable 'monitoring_id'

--- philo/sources/executor/ft_start_dinner.c
+++ philo/sources/executor/ft_start_dinner.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 09:50:46 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/20 17:17:41 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/21 11:34:00 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	ft_start_dinner(t_rules *rules)
 {
 	int			i;
-	pthread_t	thread;
 
 	i = -1;
 	ft_stopwatch(&rules->starting_stopwatch);
@@ -28,25 +27,29 @@ int	ft_start_dinner(t_rules *rules)
 			printf(RED"Error: Failed to create a thread for philo\n"RESET);
 			return (FALSE);
 		}
-		if (pthread_create(&thread, NULL, ft_monitoring, \
-			&rules->philosophers[i]))
+		if (rules->meals_per_philo == -1)
 		{
-			printf(RED"Error: Failed to create a thread for monitor\n"RESET);
-			return (FALSE);
+			if (pthread_create(&rules->philosophers->monitoring_id, NULL, \
+				ft_monitoring, &rules->philosophers[i]))
+			{
+				printf(RED"Error: Failed to create a thread for monitor\n"RESET);
+				return (FALSE);
+			}
+			pthread_detach(rules->philosophers->monitoring_id);
+			pthread_join(rules->philosophers->monitoring_id, NULL);
 		}
 	}
 	if (rules->meals_per_philo != -1)
 	{
-		if (pthread_create(&thread, NULL, ft_monitoring_each_must_each,
-			rules))
+		if (pthread_create(&rules->philosophers->monitoring_id, NULL, \
+			ft_monitoring_each_must_each, rules))
 		{
 			printf(RED"Error: Failed to create a thread for monitor\n"RESET);
 			return (FALSE);
 		}
+		pthread_detach(rules->philosophers->monitoring_id);
+		pthread_join(rules->philosophers->monitoring_id, NULL);
 	}
-	// ft_end_dinner(rules);
-	i = -1;
-	while (++i < rules->number_of_philo)
-		pthread_join(rules->philosophers[i].thread_id, NULL);
+	ft_end_dinner(rules);
 	return (TRUE);
 }
