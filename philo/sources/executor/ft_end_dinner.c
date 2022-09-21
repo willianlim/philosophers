ef22cdb803b9780180924d2908f2c3c3379467b4
Willian-code <willianrosendodelima@outlook.com>
2022-09-21 20:30:50 -0300

    synchronizing the threads

--- philo/sources/executor/ft_end_dinner.c
+++ philo/sources/executor/ft_end_dinner.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 22:04:43 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/20 16:29:23 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/21 11:48:13 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	ft_end_dinner(t_rules *rules)
 {
 	int	i;
 
-	// i = -1;
-	// while (++i < rules->number_of_philo)
-	// 	pthread_join(rules->philosophers[i].thread_id, NULL);
+	i = -1;
+	while (++i < rules->number_of_philo)
+		pthread_join(rules->philosophers[i].thread_id, NULL);
 	i = -1;
 	while (++i < rules->number_of_philo)
 		pthread_mutex_destroy(&rules->philosophers[i].check_mutex);
@@ -26,12 +26,6 @@ int	ft_end_dinner(t_rules *rules)
 	while (++i < rules->number_of_philo)
 		pthread_mutex_destroy(&rules->forks[i]);
 	pthread_mutex_destroy(&rules->finish_mutex);
-	// i = -1;
-	// while (++i < rules->number_of_philo)
-	// {
-	// 	free(rules->forks);
-	// 	free(rules->philosophers);
-	// }
 	free(rules->forks);
 	free(rules->philosophers);
 	return (TRUE);
