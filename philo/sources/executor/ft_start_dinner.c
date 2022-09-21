4531a0a3a2689f86bf30ce423ee0b28682c68377
Willian-code <willianrosendodelima@outlook.com>
2022-09-20 21:31:19 -0300

    starting the meal creating the threads and doing the monitoring

--- philo/sources/executor/ft_start_dinner.c
+++ philo/sources/executor/ft_start_dinner.c
@@ -6,57 +6,47 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 09:50:46 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/16 14:24:17 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/20 17:17:41 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_philo.h"
 
-static void	ft_finish_dinner(t_data *data, t_philosopher *philo)
+int	ft_start_dinner(t_rules *rules)
 {
-	int	i;
+	int			i;
+	pthread_t	thread;
 
 	i = -1;
-	while (++i < data->number_of_philo)
+	ft_stopwatch(&rules->starting_stopwatch);
+	while (++i < rules->number_of_philo)
 	{
-		printf("estou no destroy\n");
-		pthread_mutex_destroy(&data->fork[i]);
-	}
-	pthread_mutex_destroy(&data->lock_dinner);
-	pthread_mutex_destroy(&data->lock_print);
-	// i = -1;
-	// while (++i < data->number_of_philo)
-	// {
-	// 	free(&data->fork);
-	// 	free(&data->philosopher);
-	// }
-	free(data->fork);
-	free(data->philosopher);
-	free(philo);
-	// free(philo->data->fork);
-	// free(philo->data->philosopher);
-}
-
-int	ft_start_dinner(t_data *data, t_philosopher *philo)
-{
-	int	i;
-
-	i = -1;
-	memset(philo, 0, sizeof(t_philosopher));
-	philo = data->philosopher;
-	ft_stopwatch(&data->starting_stopwatch);
-	while (++i < data->number_of_philo)
-	{
-		if (pthread_create(&philo[i].thread, NULL, ft_routine, &philo[i]))
+		rules->philosophers[i].last_time_to_eat = rules->starting_stopwatch;
+		if (pthread_create(&rules->philosophers[i].thread_id, NULL, \
+			ft_routine, &rules->philosophers[i]))
 		{
 			printf(RED"Error: Failed to create a thread for philo\n"RESET);
 			return (FALSE);
 		}
-		ft_stopwatch(&philo[i].last_meal);
+		if (pthread_create(&thread, NULL, ft_monitoring, \
+			&rules->philosophers[i]))
+		{
+			printf(RED"Error: Failed to create a thread for monitor\n"RESET);
+			return (FALSE);
+		}
 	}
-	if (ft_monitoring(data, data->philosopher))
-		ft_end_dinner(data, philo);
-	else
-		ft_finish_dinner(data, philo);
+	if (rules->meals_per_philo != -1)
+	{
+		if (pthread_create(&thread, NULL, ft_monitoring_each_must_each,
+			rules))
+		{
+			printf(RED"Error: Failed to create a thread for monitor\n"RESET);
+			return (FALSE);
+		}
+	}
+	// ft_end_dinner(rules);
+	i = -1;
+	while (++i < rules->number_of_philo)
+		pthread_join(rules->philosophers[i].thread_id, NULL);
 	return (TRUE);
 }
