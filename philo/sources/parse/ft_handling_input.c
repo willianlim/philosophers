ec1c6abad722d2589e3a44c9fd1da570704ab099
Willian-code <willianrosendodelima@outlook.com>
2022-09-20 21:19:45 -0300

    changing 'data' to 'rules'

--- philo/sources/parse/ft_handling_input.c
+++ philo/sources/parse/ft_handling_input.c
@@ -6,13 +6,13 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 15:33:15 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/15 18:02:01 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/19 14:06:08 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_philo.h"
 
-static int	ft_init_struct(int index, char *argv, t_data *data)
+static int	ft_init_struct(int index, char *argv, t_rules *rules)
 {
 	int	number;
 
@@ -20,21 +20,21 @@ static int	ft_init_struct(int index, char *argv, t_data *data)
 	if (number > 0)
 	{
 		if (index == NUMBER_OF_PHILO)
-			data->number_of_philo = number;
+			rules->number_of_philo = number;
 		if (index == TIME_TO_DIE)
-			data->time_to_die = number;
+			rules->time_to_die = number;
 		if (index == TIME_TO_EAT)
-			data->time_to_eat = number;
+			rules->time_to_eat = number;
 		if (index == TIME_TO_SLEEP)
-			data->time_to_sleep = number;
+			rules->time_to_sleep = number;
 		if (index == MEALS_PER_PERSON)
-			data->meals_per_person = number;
+			rules->meals_per_philo = number;
 		return (TRUE);
 	}
 	return (FALSE);
 }
 
-static int	ft_integer_argument_values(int argc, char *argv[], t_data *data)
+static int	ft_integer_argument_values(int argc, char *argv[], t_rules *rules)
 {
 	int	index;
 	int	with_optional_argv;
@@ -44,23 +44,24 @@ static int	ft_integer_argument_values(int argc, char *argv[], t_data *data)
 	with_optional_argv = 6;
 	mandatory_argv_total = 4;
 	while (++index <= mandatory_argv_total)
-		if (!ft_init_struct(index, argv[index], data))
+		if (!ft_init_struct(index, argv[index], rules))
 			return (FALSE);
-	if (argc == with_optional_argv && !ft_init_struct(index, argv[index], data))
+	if (argc == with_optional_argv && \
+	!ft_init_struct(index, argv[index], rules))
 		return (FALSE);
 	else if (argc == mandatory_argv_total + 1)
-		data->meals_per_person = -1;
+		rules->meals_per_philo = -1;
 	return (TRUE);
 }
 
-int	ft_handling_input(int argc, char *argv[], t_data *data)
+int	ft_handling_input(int argc, char *argv[], t_rules *rules)
 {
 	if (argc < 5 || argc > 6)
 	{
 		printf(RED"Error: Number of arguments not allowed\n"RESET);
 		return (FALSE);
 	}
-	if (!ft_integer_argument_values(argc, argv, data))
+	if (!ft_integer_argument_values(argc, argv, rules))
 	{
 		printf(RED"Error: "RESET);
 		printf(RED"One of the values entered is not a positive integer\n"RESET);
