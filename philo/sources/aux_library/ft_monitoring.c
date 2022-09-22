286fce5df9515ab6912804af96c03abd0807b3ae
Willian-code <willianrosendodelima@outlook.com>
2022-09-22 20:30:27 -0300

    putting a delay on the monitoring function

--- philo/sources/aux_library/ft_monitoring.c
+++ philo/sources/aux_library/ft_monitoring.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 13:58:46 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/21 11:24:53 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/22 20:24:01 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	*ft_monitoring_each_must_each(void *arg)
 
 void	*ft_monitoring(void *arg)
 {
-	long long		current;
+	long			current;
 	t_philosopher	*philo;
 
 	philo = (t_philosopher *)arg;
@@ -41,13 +41,14 @@ void	*ft_monitoring(void *arg)
 		if ((current - philo->last_time_to_eat) >= philo->rules->time_to_die \
 			&& !philo->rules->finish)
 		{
-			printf("%lli\t", current - philo->rules->starting_stopwatch);
+			printf("%li\t", current - philo->rules->starting_stopwatch);
 			printf("%i\t", philo->id + 1);
 			printf(RED"died\n"RESET);
 			philo->rules->finish = TRUE;
 		}
 		pthread_mutex_unlock(&philo->rules->finish_mutex);
 		pthread_mutex_unlock(&philo->check_mutex);
+		ft_elapsed_time_sleep(2);
 	}
 	return (NULL);
 }
