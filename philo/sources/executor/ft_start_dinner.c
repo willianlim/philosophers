7620abb7aa43b8757e8b4de4f01f43b4eeb501b6
Willian-code <willianrosendodelima@outlook.com>
2022-09-22 12:35:19 -0300

    putting within the norm

--- philo/sources/executor/ft_start_dinner.c
+++ philo/sources/executor/ft_start_dinner.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 09:50:46 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/21 11:34:00 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/22 12:18:16 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,26 @@
 
 int	ft_start_dinner(t_rules *rules)
 {
-	int			i;
+	int	i;
 
 	i = -1;
-	ft_stopwatch(&rules->starting_stopwatch);
 	while (++i < rules->number_of_philo)
 	{
 		rules->philosophers[i].last_time_to_eat = rules->starting_stopwatch;
-		if (pthread_create(&rules->philosophers[i].thread_id, NULL, \
-			ft_routine, &rules->philosophers[i]))
-		{
-			printf(RED"Error: Failed to create a thread for philo\n"RESET);
-			return (FALSE);
-		}
+		pthread_create(&rules->philosophers[i].thread_id, NULL, ft_routine, \
+		&rules->philosophers[i]);
 		if (rules->meals_per_philo == -1)
 		{
-			if (pthread_create(&rules->philosophers->monitoring_id, NULL, \
-				ft_monitoring, &rules->philosophers[i]))
-			{
-				printf(RED"Error: Failed to create a thread for monitor\n"RESET);
-				return (FALSE);
-			}
+			pthread_create(&rules->philosophers->monitoring_id, NULL, \
+			ft_monitoring, &rules->philosophers[i]);
 			pthread_detach(rules->philosophers->monitoring_id);
 			pthread_join(rules->philosophers->monitoring_id, NULL);
 		}
 	}
 	if (rules->meals_per_philo != -1)
 	{
-		if (pthread_create(&rules->philosophers->monitoring_id, NULL, \
-			ft_monitoring_each_must_each, rules))
-		{
-			printf(RED"Error: Failed to create a thread for monitor\n"RESET);
-			return (FALSE);
-		}
+		pthread_create(&rules->philosophers->monitoring_id, NULL, \
+		ft_monitoring_each_must_each, rules);
 		pthread_detach(rules->philosophers->monitoring_id);
 		pthread_join(rules->philosophers->monitoring_id, NULL);
 	}
