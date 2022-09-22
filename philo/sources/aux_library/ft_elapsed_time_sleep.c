03abb22a512c68cd51415bf425139a3f66e3bb01
Willian-code <willianrosendodelima@outlook.com>
2022-09-22 20:23:20 -0300

    changing the data type of the function parameter

--- philo/sources/aux_library/ft_elapsed_time_sleep.c
+++ philo/sources/aux_library/ft_elapsed_time_sleep.c
@@ -6,7 +6,7 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 10:00:03 by wrosendo          #+#    #+#             */
-/*   Updated: 2022/09/22 10:10:21 by wrosendo         ###   ########.fr       */
+/*   Updated: 2022/09/22 15:25:32 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	ft_elapsed_time_sleep(int time_to_sleep)
 {
-	long long	end;
-	long long	start;
+	long	end;
+	long	start;
 
 	ft_stopwatch(&start);
 	ft_stopwatch(&end);
-	while (end - start < (long long)time_to_sleep)
+	while ((end - start) < (long)time_to_sleep)
 	{
-		ft_stopwatch(&end);
 		usleep(10);
+		ft_stopwatch(&end);
 	}
 }
